3b6df733558faff937cf9a2d90ba4d6a43100513
dianasribeiro <dianasribeiroti@gmail.com>
2025-09-19 18:09:47 -0300

    consertando git

--- src/br/serratec/projeto/model/FolhaPagamento.java
+++ src/br/serratec/projeto/model/FolhaPagamento.java
@@ -2,6 +2,8 @@ package br.serratec.projeto.model;
 
 import java.time.LocalDate;
 
+////// to do 
+///
 public class FolhaPagamento {
 	private int codigo;
 	private Funcionario funcionario; // relacionamento com funcionario
