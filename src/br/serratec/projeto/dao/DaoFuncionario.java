b192a5b48a6c981c784a2e3199ff7b941947d447
brunomarchiori-git <bruno.marchiori.git@gmail.com>
2025-09-19 18:25:19 -0300

    Classe DaoFuncionario, Funcionario e o gitignore

--- src/br/serratec/projeto/dao/DaoFuncionario.java
+++ src/br/serratec/projeto/dao/DaoFuncionario.java
@@ -1,5 +1,36 @@
 package br.serratec.projeto.dao;
 
-public class DaoFuncionario {
-	// TODO
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import br.serratec.projeto.model.Funcionario;
+
+	public class DaoFuncionario{
+		
+		Connection connection;
+		
+	    public DaoFuncionario(Connection connection) {
+	    	this.connection = connection;
+	    }
+
+	    public void inserir(Funcionario funcionario) {
+	        String sql = "insert into tb_funcionarios (nome_do_funcionario, cpf_do_funcionario, data_nascimento_do_funcionario, salario_bruto_do_funcionario) values (?, ?, ?, ?)";
+
+	        try {
+	            PreparedStatement stmt = connection.prepareStatement(sql);
+
+	            stmt.setString(1, funcionario.getNome());
+	            stmt.setString(2, funcionario.getCpf());
+	            stmt.setDate(3, java.sql.Date.valueOf(funcionario.getDataNascimento()));
+	            stmt.setDouble(4, funcionario.getSalarioBruto());
+
+	            stmt.execute();
+	            stmt.close();
+
+	            System.out.println("Funcionario inserido com sucesso!");
+	        }catch (SQLException e) {
+	            throw new RuntimeException(e);
+	        }
+	}
 }
--- src/br/serratec/projeto/model/Funcionario.java
+++ src/br/serratec/projeto/model/Funcionario.java
@@ -1,5 +1,75 @@
 package br.serratec.projeto.model;
 
-public class Funcionario {
-	// TODO
+import java.time.LocalDate;
+import java.util.List;
+
+public class Funcionario extends Pessoa {
+
+	private Double salarioBruto, descontoInss, descontoIr, baseIR;
+	private List<Dependente> dependentes;
+
+	public Funcionario(String nome, String cpf, LocalDate dataNascimento, Double salarioBruto) {
+		super(nome, cpf, dataNascimento);
+		this.salarioBruto = salarioBruto;
+	}
+
+	public Funcionario(String nome, String cpf, LocalDate dataNascimento) {
+		super(nome, cpf, dataNascimento);
+	}
+
+	public Double getSalarioBruto() {
+		return salarioBruto;
+	}
+
+	public Double getDescontoInss() {
+		return descontoInss;
+	}
+
+	public Double getDescontoIr() {
+		return descontoIr;
+	}
+	
+	public Double getBaseIR() {
+		return baseIR;
+	}
+
+	public Double CalcularInss(Double salarioBruto) {
+		if(salarioBruto > 8157.41) {
+			descontoInss = (salarioBruto*0.14);
+			baseIR = (salarioBruto - descontoInss) - 190.42;
+			return baseIR;
+		}else if(salarioBruto > 4190.83) {
+			descontoInss = (salarioBruto*0.14);
+			baseIR = (salarioBruto - descontoInss) - 190.42;
+			return baseIR; 
+		}else if(salarioBruto > 2793.88) {
+			descontoInss = (salarioBruto*0.12);
+			baseIR = (salarioBruto - descontoInss) - 106.60;
+			return baseIR; 
+		}else if(salarioBruto > 1518.00) {
+			descontoInss = (salarioBruto*0.09);
+			baseIR = (salarioBruto - descontoInss) - 22.77;
+			return baseIR; 
+		}else {
+			 descontoInss = (salarioBruto*0.075);
+			 baseIR = (salarioBruto - descontoInss);
+			 return baseIR;
+		}
+	}
+	
+	public Double CalcularIr(Double baseIR) {
+		double valorDependentes = dependentes.size() * 189.59;
+		if(salarioBruto > 4664.68) {
+			return descontoIr = (baseIR*0.275) - (valorDependentes);
+		}else if(salarioBruto > 3751.05) {
+			return descontoIr = (baseIR*0.225) - (valorDependentes);
+		}else if(salarioBruto > 2826.66) {
+			return descontoIr = (baseIR*0.15) - (valorDependentes);
+		}else if(salarioBruto > 2259.21) {
+			return descontoIr = (baseIR*0.075) - (valorDependentes);
+		}else {
+			return descontoIr = 0.00;
+		}
+	}
+	
 }
