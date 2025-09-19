b45d8ba9b70614c35c85bf3eace8e5cf4b294fb8
jessicamaglianoti <jessicamagliano.ti@gmail.com>
2025-09-19 09:40:53 -0300

    Implementação da classe Pessoa

--- src/br/serratec/projeto/model/Pessoa.java
+++ src/br/serratec/projeto/model/Pessoa.java
@@ -1,5 +1,33 @@
 package br.serratec.projeto.model;
 
+import java.time.LocalDate;
+
 public abstract class Pessoa {
-	// TODO
+
+	protected String nome;
+	protected String cpf;
+	protected LocalDate dataNascimento;
+
+	public Pessoa(String nome, String cpf, LocalDate dataNascimento) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getNome() {
+		return nome;
+
+	}
+
+	public String getCpf() {
+		return cpf;
+
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+
+	}
+
 }
